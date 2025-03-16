b1a3956ec9268b20cd0d81ae6c9feb9156a79c06
laboso820 <obadiahlaboso820@gmail.com>
2025-03-16 23:23:52 +0300

    modified js

--- main.js
+++ main.js
@@ -3,3 +3,4 @@ console.log(myname)
 console.log(typeof myname)
 document.getElementById("navbar").style.color="red";
 document.getElementById("home").style.textBoxcolor="brown";
+console.log("this is new js")
