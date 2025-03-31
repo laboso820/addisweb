88c347b77b105d9cadb98c6c08929f22979f3968
laboso820 <obadiahlaboso820@gmail.com>
2025-03-31 21:40:29 +0300

    js add

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-let myname= "obadiah"
-console.log(myname)
-console.log(typeof myname)
-document.getElementById("navbar").style.color="red";
-document.getElementById("home").style.textBoxcolor="brown";
-console.log("this is new js")
+// let myname= "obadiah"
+// console.log(myname)
+// console.log(typeof myname)
+// document.getElementById("navbar").style.color="red";
+// document.getElementById("home").style.textBoxcolor="brown";
+// console.log("this is new js")
+function password(){
+    const uppercase="ABCDEFGHIJKLMNOPQRSTUVWXTZ"
+    const lowercase="abcdefghjklmnopqrstuvwxyz"
+    const numbers="0123456789"
+}
+password
