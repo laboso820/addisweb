b72d473462467cc68980a05b73377383b94043f8
laboso820 <obadiahlaboso820@gmail.com>
2025-03-31 21:41:49 +0300

    new changes

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-// let myname= "obadiah"
-// console.log(myname)
-// console.log(typeof myname)
-// document.getElementById("navbar").style.color="red";
-// document.getElementById("home").style.textBoxcolor="brown";
-// console.log("this is new js")
+let myname= "obadiah"
+console.log(myname)
+console.log(typeof myname)
+document.getElementById("navbar").style.color="red";
+document.getElementById("home").style.textBoxcolor="brown";
+console.log("this is new js")
 function password(){
     const uppercase="ABCDEFGHIJKLMNOPQRSTUVWXTZ"
     const lowercase="abcdefghjklmnopqrstuvwxyz"
